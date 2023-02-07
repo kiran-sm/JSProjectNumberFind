24a5f3a6c45bcfe4ef7cc763cc82971a95a91a8d
kiran <smkiran.09@gmail.com>
2023-02-07 21:57:07 +0530

    End of 74

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,48 @@
-console.log("test");
+// console.log(document.querySelector(".message").textContent);    
+// document.querySelector(".message").textContent = '🎉 Correct Number!'   
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 20;
+
+// document.querySelector(".guess").value=23
+// console.log(document.querySelector(".guess").value);
+
+const secreteNumber = Math.trunc(Math.random()*20)+1;
+let score = 20;
+document.querySelector(".number").textContent = secreteNumber;
+
+document.querySelector(".check").addEventListener('click',function(){
+    const guess = Number(document.querySelector(".guess").value);
+    console.log(guess, typeof (guess));
+
+    if(!guess){
+        document.querySelector(".message").textContent = '⛔️Not a Wrong Number!' 
+    }
+    else if(guess === secreteNumber){
+        document.querySelector(".message").textContent = '🎉 Correct Number!'
+    }
+    else if(guess > secreteNumber){
+        if(score>1){
+            document.querySelector(".message").textContent = '📈 Too high!';
+        score--;
+        document.querySelector(".score").textContent = score;
+        }
+
+        else{
+        document.querySelector(".score").textContent = 0;
+        document.querySelector(".message").textContent = '💥 You lost the game'
+        }
+        
+
+    }
+    else if(guess < secreteNumber){
+        if(score>1){
+            document.querySelector(".message").textContent = '📉 Too low!'
+        score--;
+        document.querySelector(".score").textContent = score;
+        }
+        else{
+            document.querySelector(".score").textContent = 0;
+            document.querySelector(".message").textContent = '💥 You lost the game'
+            }
+    }
+});
