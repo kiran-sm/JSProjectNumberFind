a8a4ffc6b7c1ecf0dc85690f2a1adcb960ea5e69
kiran <smkiran.09@gmail.com>
2023-02-08 07:33:03 +0530

    78 Completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,19 +6,26 @@
 // document.querySelector(".guess").value=23
 // console.log(document.querySelector(".guess").value);
 
-const secreteNumber = Math.trunc(Math.random()*20)+1;
+let secreteNumber = Math.trunc(Math.random()*20)+1;
 let score = 20;
-document.querySelector(".number").textContent = secreteNumber;
+let highScore = 0;
 
 document.querySelector(".check").addEventListener('click',function(){
     const guess = Number(document.querySelector(".guess").value);
-    console.log(guess, typeof (guess));
+    
 
     if(!guess){
         document.querySelector(".message").textContent = '⛔️Not a Wrong Number!' 
     }
     else if(guess === secreteNumber){
         document.querySelector(".message").textContent = '🎉 Correct Number!'
+        document.querySelector("body").style.background="#60b347";
+document.querySelector(".number").textContent = secreteNumber;
+document.querySelector(".number").style.width="30rem";
+if(score > highScore){
+    highScore=score;
+    document.querySelector(".highscore").textContent=highScore;
+}
     }
     else if(guess > secreteNumber){
         if(score>1){
@@ -46,3 +53,15 @@ document.querySelector(".check").addEventListener('click',function(){
             }
     }
 });
+
+document.querySelector(".again").addEventListener("click", function(){
+    score = 20;
+let secreteNumber = Math.trunc(Math.random()*20)+1;
+document.querySelector(".message").textContent = 'Start Guessing...';
+document.querySelector(".score").textContent = score;
+document.querySelector(".number").textContent = '?';
+document.querySelector(".guess").textContent = '';
+document.querySelector("body").style.background="#222";
+document.querySelector(".number").style.width="15rem";
+
+})
